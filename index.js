b420c584d50c927264546f501f91c72d6dfbaafb
BehinaBhr <be.bhr77@gmail.com>
2024-12-12 13:20:51 -0800

    Install 'heroku-ssl-redirect' and configure middleware to enable SSL redirection

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const linksRoutes = require("./routes/links-routes");
 const albumsRoutes = require("./routes/albums-routes");
 const eventsRoutes = require("./routes/events-routes");
 const adminRoutes = require("./routes/admin-routes");
+const sslRedirect = require('heroku-ssl-redirect').default;
 require("dotenv").config();
 
 const app = express();
@@ -15,6 +16,8 @@ const CORS_ORIGIN = process.env.CORS_ORIGIN || "http://localhost:3000";
 // Middleware to link API to frontend
 app.use(cors({ origin: CORS_ORIGIN }));
 app.use(express.json());
+// Middleware to enable SSL redirect
+app.use(sslRedirect());
 
 app.use("/api/admin", adminRoutes);
 app.use("/api/groups", groupsRoutes);
